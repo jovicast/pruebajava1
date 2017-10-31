4689125adafa18930bbb496e16c7d1339f137436
jovicast <myriamcastillo630@gmail.com>
2017-10-30 21:42:40 -0300

    calcular el area de base y altura

---
+++ src/puebajava1/CalcularArea.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puebajava1;
+
+import javax.swing.JOptionPane;
+
+/************************************
+ *Myriam Castillo Zavala;
+ * @author LAB07
+ * Fecha: Octubre 30 de 2017
+ * Programa: Clase3Myriam
+ * Responsabilidad: Clase Principal
+ ***********************************/
+public class CalcularArea 
+        
+{
+    int area;
+    int base;
+    int altura;
+
+  public static void main(String[] args)
+  {       
+    // TODO CODIGO DEBE APLICARSE AQUI
+   System.out.println("ingrese el valor de base");
+   System.out.println("ingrese el valor de altura");
+   System.out.println("Calcular el area=base*altura");
+   
+        String showInputDialog;
+        
+        showInputDialog = JOptionPane.showInputDialog(null,"calcula el area=base*altura ");
+  
+ }
+    }
+   
+
