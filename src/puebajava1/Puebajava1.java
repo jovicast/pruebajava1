0268359c003cd98b7e9c125777afa0ae6a416d8f
jovicast <myriamcastillo630@gmail.com>
2017-10-30 21:06:40 -0300

    ingresar los datos de proyecto nuevo puebajava1

---
+++ src/puebajava1/Puebajava1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puebajava1;
+
+/************************************
+ * Myriam Castillo Zavala;
+ * @author LAB07
+ * Fecha: Octubre 30 de 2017
+ * Programa: Clase3Myriam
+ * Responsabilidad: Clase Principal
+ ************************************/
+public class Puebajava1 
+{
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[]args)
+     {
+        //TODO code application logic here
+    }
+    
+    
+    
+}
